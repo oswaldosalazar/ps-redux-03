a32e0b9e4e2b5648048806d2a787419e1568b0cd
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-07-12 21:48:00 -0600

    Initialized state in Course Page component

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ''
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  render() {
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+export default CoursesPage;
